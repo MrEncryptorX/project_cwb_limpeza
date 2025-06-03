5f3a94df7a56519f61c860c1dda28f4fa776f19b
MrEncryptorX <matheuscavalheiro42@gmail.com>
2025-06-03 15:43:43 -0300

    correções de imagens

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../assets/Logo.png';
+import Logo from '/public/assets/Logo.png';
 import { Check, Star } from 'lucide-react';
 
 const About: React.FC = () => {
